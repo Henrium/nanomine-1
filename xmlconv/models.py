ea3971686d0ee89079ed03c9d0ab917777ab156b
NanoMine Admin <nanominenu@gmail.com>
2017-04-26 17:49:42 -0400

    first commit

---
+++ xmlconv/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils.encoding import smart_unicode
+
+# Create your models here.
+class Document(models.Model):
+    docfile = models.FileField(upload_to='./xmlconv/media')
+    
