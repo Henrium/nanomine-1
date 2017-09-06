02500d10064cfe4ecc22c2e82ac53d45d9f543cf
NanoMine Admin <nanominenu@gmail.com>
2017-09-05 23:56:03 -0400

    file clean up

--- exporter/curator/blob/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-   url('', 'exporter.curator.blob.models.BLOBExporter', {'name': 'BLOB', 'available_for_all': True}),
-)
-
--- niblack/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class DocumentForm(forms.Form):
-    docfile = forms.FileField(
-        label='Select file'
-    )
-    
-class NiblackAdjustForm(forms.Form):
-    adjust = forms.CharField(label='Enter Pixel Size to Adjust:', max_length=100)    
